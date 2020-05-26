ce7bbc147db585cfd515a279b51f897f5db972ef
Kristina <codingkris@yahoo.com>
2020-05-26 15:35:30 +0300

    Font Awesome added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "@fortawesome/fontawesome-free/js/all";
 import "../stylesheets/application.scss";
 import "./navbar.js";
 import "./notification.js";
