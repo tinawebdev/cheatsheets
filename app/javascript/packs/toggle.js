5a0e5be11462de2084952e2f8bcab193cfaab205
Kristina <codingkris@yahoo.com>
2020-05-30 12:36:03 +0300

    Toggle added

---
+++ app/javascript/packs/toggle.js
@@ -0,0 +1,8 @@
+document.addEventListener("turbolinks:load", () => {
+  const arrow = document.querySelector('#arrow');
+  const description = document.querySelector('.description');
+
+  arrow.onclick = function() {
+    description.classList.toggle('is-hidden');
+  };
+});
