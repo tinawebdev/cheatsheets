fc41a163c31338981300bb49d29198001aefa863
Kristina <codingkris@yahoo.com>
2020-05-31 17:38:40 +0300

    Toggle fixed

--- app/javascript/packs/toggle.js
+++ app/javascript/packs/toggle.js
@@ -2,8 +2,10 @@ document.addEventListener("turbolinks:load", () => {
   const arrow = document.querySelector('#arrow');
   const description = document.querySelector('.description');
 
-  arrow.onclick = function() {
-    description.classList.toggle('is-hidden');
-    arrow.classList.toggle('rotate');
-  };
+  if (arrow != null) {
+    arrow.onclick = function() {
+      description.classList.toggle('is-hidden');
+      arrow.classList.toggle('rotate');
+    };
+  }
 });
