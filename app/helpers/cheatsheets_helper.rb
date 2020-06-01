6a82967f6db4c0722c54382f3004ccce06ac650c
Kristina <codingkris@yahoo.com>
2020-06-01 12:27:44 +0300

    Added different colors for cheatsheet date tags

--- app/helpers/cheatsheets_helper.rb
+++ app/helpers/cheatsheets_helper.rb
@@ -1,7 +1,19 @@
 module CheatsheetsHelper
-  def is_updated?(cheatsheet)
+  def created_tag(cheatsheet)
+    content_tag(:span, "Created: #{cheatsheet.created_at.strftime("%B %d, %Y")}", class: "tag #{created_tag_color(cheatsheet)}")
+  end
+
+  def updated_tag(cheatsheet)
     if cheatsheet.updated_at > cheatsheet.created_at
-      content_tag(:span, "Updated: #{cheatsheet.updated_at.strftime("%B %d, %Y")}", class: "tag is-dark")
+      content_tag(:span, "Updated: #{cheatsheet.updated_at.strftime("%B %d, %Y")}", class: "tag #{updated_tag_color(cheatsheet)}")
     end
+  end
+
+  def created_tag_color(cheatsheet)
+    cheatsheet.recently_created ? 'is-success' : 'is-dark'
+  end
+
+  def updated_tag_color(cheatsheet)
+    cheatsheet.recently_updated ? 'is-success' : 'is-dark'
   end
 end
--- app/models/cheatsheet.rb
+++ app/models/cheatsheet.rb
@@ -1,3 +1,11 @@
 class Cheatsheet < ApplicationRecord
   belongs_to :user
+
+  def recently_created
+    self.created_at >= 24.hour.ago
+  end
+
+  def recently_updated
+    self.updated_at >= 3.days.ago
+  end
 end
