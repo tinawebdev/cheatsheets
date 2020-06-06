acb433feed34d715bf2a46a19b36452987a099a9
Kristina <codingkris@yahoo.com>
2020-06-06 09:53:03 +0300

    Fixed js files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,4 +10,4 @@ import "../stylesheets/application.scss";
 import "./navbar.js";
 import "./notification.js";
 import "./toggle.js";
-import "../trix-editor-overrides"
+import "./trix-editor-overrides";
