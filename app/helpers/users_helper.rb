cbc5d2aba60917090344b2e8ac23a4177776fc38
Kristina <codingkris@yahoo.com>
2020-06-15 12:26:52 +0300

    Added gravatar to users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,10 @@
 module UsersHelper
+  def gravatar_for(user, size: "120")
+    hash = Digest::MD5::hexdigest(user.email.downcase)
+    image_src = "https://secure.gravatar.com/avatar/#{hash}?d=monsterid&s=#{size}"
+    image_tag(image_src, alt: user.name, class: "user_avatar mb-2")
+  end
+
   def has_name?(user, style="has-text-danger")
     if (user.name.present?)
       name = content_tag(:span, "#{user.name} ", class: style)
