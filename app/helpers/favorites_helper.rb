d3b661a0bbad75e18c2da4c3f34bc6e0fbc4c04a
Kristina <codingkris@yahoo.com>
2020-06-17 12:30:04 +0300

    Favorites button fixed

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,2 +1,16 @@
 module FavoritesHelper
+  def add_or_remove_favorite(cheatsheet, styles: "")
+    icon_bookmark_filled = content_tag(:i, '', class: "fas fa-bookmark")
+    icon_bookmark_empty = content_tag(:i, '', class: "far fa-bookmark")
+
+    if current_user && current_user.favorited_cheatsheets.exists?(cheatsheet.id)
+      link_to favorite_path(cheatsheet), method: :delete, class: styles do
+        content_tag(:span, icon_bookmark_filled, class: "icon is-small has-text-danger")
+      end
+    else
+      link_to favorites_path(cheatsheet_id: cheatsheet), method: :post, class: styles do
+        content_tag(:span, icon_bookmark_empty, class: "favorite icon is-small has-text-grey")
+      end
+    end
+  end
 end
