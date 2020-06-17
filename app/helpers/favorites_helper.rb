7eca2c445e61686b34f2d34fb4ef8e80f101f35d
Kristina <codingkris@yahoo.com>
2020-06-17 16:58:15 +0300

    Favorite add/remove button fixed

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -3,13 +3,15 @@ module FavoritesHelper
     icon_bookmark_filled = content_tag(:i, '', class: "fas fa-bookmark")
     icon_bookmark_empty = content_tag(:i, '', class: "far fa-bookmark")
 
-    if current_user && current_user.favorited_cheatsheets.exists?(cheatsheet.id)
-      link_to favorite_path(cheatsheet), method: :delete, class: styles do
-        content_tag(:span, icon_bookmark_filled, class: "icon is-small has-text-danger")
-      end
-    else
-      link_to favorites_path(cheatsheet_id: cheatsheet), method: :post, class: styles do
-        content_tag(:span, icon_bookmark_empty, class: "favorite icon is-small has-text-grey")
+    if current_user 
+      if current_user.favorited_cheatsheets.exists?(cheatsheet.id)
+        link_to favorite_path(cheatsheet), method: :delete, class: styles do
+          content_tag(:span, icon_bookmark_filled, class: "icon is-small has-text-danger")
+        end
+      else
+        link_to favorites_path(cheatsheet_id: cheatsheet), method: :post, class: styles do
+          content_tag(:span, icon_bookmark_empty, class: "favorite icon is-small has-text-grey")
+        end
       end
     end
   end
